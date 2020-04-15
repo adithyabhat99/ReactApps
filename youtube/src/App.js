9f7902ac08c7d9e1815adb32b6854416c42e5f4c
adithyabhat99 <adithyabhatoct@gmail.com>
2020-04-15 19:51:23 +0530

    added youtube clone

---
+++ youtube/src/App.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+import youtube from "./api/youtube";
+import { SearchBar, VideoList, VideoDetail } from "./components";
+
+class App extends React.Component{
+
+    state={
+        videos:[],
+        selectedVideo:null,
+    }
+
+    componentDidMount(){
+        this.handleSubmit("");
+    }
+
+    handleSubmit = async (searchTerm) =>{
+        const response = await youtube.get("search",{params:{
+            part:"snippet",
+            maxResults:5,
+            key:"AIzaSyAoyfOi7nACCmZXG-tJda2h1kE8S7Zg3qU",
+            q:searchTerm,
+        }});
+        this.setState({ videos:response.data.items, selectedVideo:response.data.items[0] });
+    }
+
+    onVideoSelect = (video) =>{
+        this.setState({ selectedVideo:video });
+    }
+
+    render(){
+        const { videos, selectedVideo } = this.state;
+        return (
+            <Grid container style={{ justifyContent: "center" }} spacing={10}>
+                <Grid item xs={12}>
+                    <Grid container spacing={10}>
+                        <Grid item xs={12}>
+                            <SearchBar onFormSubmit={this.handleSubmit}/>
+                        </Grid>
+                        <Grid item xs={8}>
+                            <VideoDetail video={selectedVideo}/>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <VideoList videos={videos} onVideoSelect={this.onVideoSelect}/>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        );
+    }
+}
+
+export default App;
